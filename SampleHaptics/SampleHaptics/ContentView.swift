06bb4e7f66e6374a6268ebff1602769546da19c5
Ossamoon <osamu950623@gmail.com>
2021-05-05 17:34:29 +0900

    First Commit

---
+++ SampleHaptics/SampleHaptics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SampleHaptics
+//
+//  Created by 齋藤修 on 2021/05/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SampleHaptics/SampleHaptics/SampleHapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleHapticsApp.swift
+//  SampleHaptics
+//
+//  Created by 齋藤修 on 2021/05/05.
+//
+
+import SwiftUI
+
+@main
+struct SampleHapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
